f0506098d3773f1a8e37e27e8e9e4c62003465c9
Pietro Delfino <delfino.pietro@gmail.com>
2018-01-23 11:35:13 +0100

    Forgot to add poppr library. Restored text in help file

--- app_plotly.R
+++ app_plotly.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(plotly)
 library(knitr)
+library(poppr)
 
 # By default, the file size limit is 5MB. It can be changed by
 # setting this option. Here we'll raise limit to 9MB.
@@ -13,87 +14,87 @@ ui <- fluidPage(
   tabsetPanel(
     tabPanel("PCA",
              sidebarLayout(
-  sidebarPanel(
-    fileInput('file1', 'Choose CSV File',
-              accept=c('text/csv',
-                       'text/comma-separated-values,text/plain',
-                       '.csv')),
-    tags$hr() , 
-    checkboxInput('scale', 'Scaling:', TRUE),
-    tags$hr(),
-    checkboxInput('center', 'Centering:', TRUE),
-    tags$hr(),
-    selectInput('z', 'NA method:', choices = c("mean","asis","zero"), selected = "mean"),
-    tags$hr(),
-    sliderInput('axes', 'Number of axes retained:', min = 3, max = 50,
-                value = 3, step = 1, round = 0),
-    tags$hr(),
-    radioButtons('xax', 'Which PC on X axes:', c("1"="Axis1", "2"="Axis2"), "Axis1"),
-    tags$hr(),
-    radioButtons('yax', 'Which PC on Y axes:', c("2"="Axis2", "3"="Axis3"), "Axis2"),
-    tags$hr(),
-    textInput('title', "Plot Title", ''),
+               sidebarPanel(
+                 fileInput('file1', 'Choose CSV File',
+                           accept=c('text/csv',
+                                    'text/comma-separated-values,text/plain',
+                                    '.csv')),
+                 tags$hr() , 
+                 checkboxInput('scale', 'Scaling:', TRUE),
+                 tags$hr(),
+                 checkboxInput('center', 'Centering:', TRUE),
+                 tags$hr(),
+                 selectInput('z', 'NA method:', choices = c("mean","asis","zero"), selected = "mean"),
+                 tags$hr(),
+                 sliderInput('axes', 'Number of axes retained:', min = 3, max = 50,
+                             value = 3, step = 1, round = 0),
+                 tags$hr(),
+                 radioButtons('xax', 'Which PC on X axes:', c("1"="Axis1", "2"="Axis2"), "Axis1"),
+                 tags$hr(),
+                 radioButtons('yax', 'Which PC on Y axes:', c("2"="Axis2", "3"="Axis3"), "Axis2"),
+                 tags$hr(),
+                 textInput('title', "Plot Title", ''),
+                 
+                 width = 2),
+               
+               mainPanel(
+                 plotlyOutput("PCA",width = "100%", height = "150%")
+                 #tableOutput('table')
+               )
+             )
+    ),
+    ############## start pcoa panel ###############
+    tabPanel("PCoA",
+             sidebarLayout(
+               sidebarPanel(
+                 # fileInput('file1', 'Choose CSV File',
+                 #           accept=c('text/csv',
+                 #                    'text/comma-separated-values,text/plain',
+                 #                    '.csv')),
+                 
+                 checkboxInput('freq', 'Use allele frequency?', TRUE),
+                 tags$hr() , 
+                 selectInput('q', 'NA method:', choices = c("mean","asis","zero"), selected = "mean"),
+                 tags$hr(),
+                 selectInput('method', 'Distance calculation method:',
+                             choices = c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski"),
+                             selected = "euclidean"),
+                 tags$hr(),
+                 sliderInput('axes', 'Number of axes retained:', min = 3, max = 50,
+                             value = 3, step = 1, round = 0),
+                 tags$hr(),
+                 radioButtons('xax', 'Which PC on X axes:', c("1"="Axis1", "2"="Axis2"), "Axis1"),
+                 tags$hr(),
+                 radioButtons('yax', 'Which PC on Y axes:', c("2"="Axis2", "3"="Axis3"), "Axis2"),
+                 tags$hr(),
+                 textInput('title', "Plot Title", ''),
+                 
+                 width = 2),
+               
+               mainPanel(
+                 plotlyOutput("PCoA",width = "100%", height = "150%")
+                 #tableOutput('table')
+               )
+             )
+    ),
     
-    width = 2),
+    
+    
+    tabPanel("Help",
+             tags$h1("Tutorial", style = "color:darkblue"),
+             tags$p("In progress", style="font-size:20px"))
+    
+             ),
   
-  mainPanel(
-    plotlyOutput("PCA",width = "100%", height = "150%")
-    #tableOutput('table')
-)
-)
-),
-############## start pcoa panel ###############
-tabPanel("PCoA",
-         sidebarLayout(
-           sidebarPanel(
-             # fileInput('file1', 'Choose CSV File',
-             #           accept=c('text/csv',
-             #                    'text/comma-separated-values,text/plain',
-             #                    '.csv')),
-             
-             checkboxInput('freq', 'Use allele frequency?', TRUE),
-             tags$hr() , 
-             selectInput('q', 'NA method:', choices = c("mean","asis","zero"), selected = "mean"),
-             tags$hr(),
-             selectInput('method', 'Distance calculation method:',
-                         choices = c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski"),
-                         selected = "euclidean"),
-             tags$hr(),
-             sliderInput('axes', 'Number of axes retained:', min = 3, max = 50,
-                         value = 3, step = 1, round = 0),
-             tags$hr(),
-             radioButtons('xax', 'Which PC on X axes:', c("1"="Axis1", "2"="Axis2"), "Axis1"),
-             tags$hr(),
-             radioButtons('yax', 'Which PC on Y axes:', c("2"="Axis2", "3"="Axis3"), "Axis2"),
-             tags$hr(),
-             textInput('title', "Plot Title", ''),
-             
-             width = 2),
-           
-           mainPanel(
-             plotlyOutput("PCoA",width = "100%", height = "150%")
-             #tableOutput('table')
-           )
-         )
-),
+  tags$style(type="text/css",
+             ".shiny-output-error { visibility: hidden; }",
+             ".shiny-output-error:before { visibility: hidden; }"
+  )
+  
+    )
 
 
 
-tabPanel("Help",
-         tags$h1("In progress", style = "color:darkblue")
-         
-
-),
-
-tags$style(type="text/css",
-           ".shiny-output-error { visibility: hidden; }",
-           ".shiny-output-error:before { visibility: hidden; }"
-)
-
-)
-)
-          
-    
 #|----------------------------------------------- SERVER CODE ------------------------------------|
 
 server <- function(input, output, session) {
@@ -105,7 +106,7 @@ server <- function(input, output, session) {
     
   })
   
-
+  
   
   # observe({
   #   updateSelectInput(session,
@@ -119,10 +120,10 @@ server <- function(input, output, session) {
     dat.pca <- dudi.pca(dat.X,cent=FALSE,scale=FALSE,scannf = F,nf = input$axes)
     
     plot_ly(dat.pca$li, x=dat.pca$li[,input$xax],y=dat.pca$li[,input$yax], text=rownames(dat.pca$li),
-                mode = "markers", color = pop(dat.genind), marker = list(size = 11)) %>%
-    layout(p, title = input$title, 
-               xaxis = list(title = "PC 1"),
-              yaxis = list(title = "PC 2"))
+            mode = "markers", color = pop(dat.genind), marker = list(size = 11)) %>%
+      layout(p, title = input$title, 
+             xaxis = list(title = "PC 1"),
+             yaxis = list(title = "PC 2"))
     
   })
   
@@ -145,4 +146,3 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui = ui, server = server)
-
